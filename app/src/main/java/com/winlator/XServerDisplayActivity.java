c759d3cbc7a1c9154a1b25132d35c40c5bbed3d4
sidaodomorro <danilaferrari77@gmail.com>
2026-04-24 12:51:47 -0300

    Revert "correção3"
    This reverts commit e8ee195aa6c42a044906a700c4dfd843ae04f695.

--- app/src/main/java/com/winlator/XServerDisplayActivity.java
+++ app/src/main/java/com/winlator/XServerDisplayActivity.java
@@ -173,11 +173,7 @@ public class XServerDisplayActivity extends AppCompatActivity implements Navigat
             if (wineInfo != WineInfo.MAIN_WINE_INFO) rootFS.setWinePath(wineInfo.path);
 
             String wineprefixWineVersion = container.getExtra("wineprefixWineVersion");
-
-            if (wineprefixWineVersion.isEmpty()) {
-                container.putExtra("wineprefixWineVersion", wineVersion);
-                container.saveData();
-            } else if (!wineprefixWineVersion.equals(wineVersion)) {
+            if (!wineprefixWineVersion.equals(wineVersion)) {
                 container.putExtra("wineprefixNeedsUpdate", "t");
                 container.saveData();
             }
